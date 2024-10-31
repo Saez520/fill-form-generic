91a6279ed4e3fdf5c43546b439905eadf354a12f
Juan Saez <juan.saez@quipux.com>
2024-10-31 17:24:30 -0500

    style: :dizzy: animation apparition
    added animation added to the appearance on the form page

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -7,6 +7,7 @@ import { AlertCircle, X } from "lucide-react";
 import { toast } from "@/hooks/use-toast";
 import { Alert } from '@/components/ui/alert';
 import Dropzone from '@/components/drop-zone';
+import Navbar from '@/components/navbar';
 
 interface VariableChange {
     id: number;
@@ -95,65 +96,68 @@ export default function Component() {
     };
 
     return (
-        <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
-            <form onSubmit={handleSubmit} className="w-full max-w-lg space-y-6">
-                <Label className="text-3xl text-center font-mono">FILL FORM</Label>
+        <>
+            <Navbar />
+            <div className="min-h-screen flex items-center justify-center p-4">
+                <form onSubmit={handleSubmit} className="w-full  max-w-lg space-y-6 translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+                    <Label className="text-3xl text-center font-mono">FILL FORM</Label>
 
-                <Alert>
-                    <AlertCircle className="h-4 w-4 text-blue-400" />
-                    <h3 className="font-bold text-blue-400 mb-2">Instrucciones</h3>
-                    <div className="text-blue-400">
-                        <ul className="list-disc text-sm grid grid-cols-2 gap-x-5 gap-y-1 content-between justify-between">
-                            <li className="text-justify p-1 rounded-md hover:bg-slate-800">
-                                In your .docx file name the variables between braces, for example: <code>{`{variable}`}</code>
-                            </li>
-                            <li className='text-justify p-1 rounded-md hover:bg-slate-800'>
-                                In the <strong>Variable name</strong> field, type the name of the variable as you have it in your .docx file, for example: <code>{`variable`}</code>
-                            </li>
-                            <li className='text-justify p-1 rounded-md hover:bg-slate-800'>
-                                Do not repeat the variables, each one must have a unique name.
-                            </li>
-                            <li className='text-justify p-1 rounded-md hover:bg-slate-800'>
-                                In the <strong>Value</strong> field, type the value you want to replace the variable with.
-                            </li>
-                        </ul>
-                    </div>
-                </Alert>
-
-                {formData.changes.map(change => (
-                    <div key={change.id} className="space-y-2">
-                        <div className="flex flex-col sm:flex-row sm:items-center space-y-2 sm:space-y-0 sm:space-x-2">
-                            <Input
-                                type="text"
-                                value={change.name}
-                                onChange={(e) => updateChange(change.id, 'name', e.target.value)}
-                                placeholder="Variable name"
-                                className="flex-grow bg-gray-800 text-white"
-                            />
-                            <Input
-                                type="text"
-                                value={change.value}
-                                onChange={(e) => updateChange(change.id, 'value', e.target.value)}
-                                placeholder="Value"
-                                className="flex-grow bg-gray-800 text-white"
-                            />
-                            <Button onClick={() => removeChange(change.id)} variant="ghost" size="icon">
-                                <X className="h-4 w-4" />
-                            </Button>
+                    <Alert className='bg-gray-900'>
+                        <AlertCircle className="h-4 w-4 text-blue-400" />
+                        <h3 className="font-bold text-blue-400 mb-2">Instrucciones</h3>
+                        <div className="text-blue-400">
+                            <ul className="list-disc text-sm grid grid-cols-2 gap-x-5 gap-y-1 content-between justify-between">
+                                <li className="text-justify p-1 rounded-md hover:bg-slate-800">
+                                    In your .docx file name the variables between braces, for example: <code>{`{variable}`}</code>
+                                </li>
+                                <li className='text-justify p-1 rounded-md hover:bg-slate-800'>
+                                    In the <strong>Variable name</strong> field, type the name of the variable as you have it in your .docx file, for example: <code>{`variable`}</code>
+                                </li>
+                                <li className='text-justify p-1 rounded-md hover:bg-slate-800'>
+                                    Do not repeat the variables, each one must have a unique name.
+                                </li>
+                                <li className='text-justify p-1 rounded-md hover:bg-slate-800'>
+                                    In the <strong>Value</strong> field, type the value you want to replace the variable with.
+                                </li>
+                            </ul>
                         </div>
-                    </div>
-                ))}
+                    </Alert>
 
-                <Button type="button" onClick={addChange} className="w-full bg-blue-500 hover:bg-blue-700">
-                    Add Variable
-                </Button>
+                    {formData.changes.map(change => (
+                        <div key={change.id} className="space-y-2">
+                            <div className="flex flex-col sm:flex-row sm:items-center space-y-2 sm:space-y-0 sm:space-x-2">
+                                <Input
+                                    type="text"
+                                    value={change.name}
+                                    onChange={(e) => updateChange(change.id, 'name', e.target.value)}
+                                    placeholder="Variable name"
+                                    className="flex-grow bg-gray-800 text-white"
+                                />
+                                <Input
+                                    type="text"
+                                    value={change.value}
+                                    onChange={(e) => updateChange(change.id, 'value', e.target.value)}
+                                    placeholder="Value"
+                                    className="flex-grow bg-gray-800 text-white"
+                                />
+                                <Button onClick={() => removeChange(change.id)} variant="ghost" size="icon">
+                                    <X className="h-4 w-4" />
+                                </Button>
+                            </div>
+                        </div>
+                    ))}
 
-                <Dropzone onFileUpload={handleFileUpload} />
+                    <Button type="button" onClick={addChange} className="w-full bg-blue-500 hover:bg-blue-700">
+                        Add Variable
+                    </Button>
 
-                <Button type="submit" className="w-full bg-green-500 hover:bg-green-700">
-                    Submit Changes
-                </Button>
-            </form>
-        </div>
+                    <Dropzone onFileUpload={handleFileUpload} />
+
+                    <Button type="submit" className="w-full bg-green-500 hover:bg-green-700">
+                        Submit Changes
+                    </Button>
+                </form>
+            </div>
+        </>
     );
 }
